a6f0f3bdbf2e9b65a7e66318f24a0eaf23a6d73c
Bailey Foltz <bailey@Baileys-MacBook.lan>
2022-12-01 23:20:02 -0500

    first commit

---
+++ assets/js/carousel.js
@@ -0,0 +1,101 @@
+// okay this code is much longer than it needs to be i am aware but bear with me i am not refactoring it
+// this is how i understand it
+// code is from a youtube video by kevin powell https://www.youtube.com/watch?v=gBzsE0oieio&t=23s
+// but obviously adjusted slightly because that code only works if there's one carousel on the page
+
+const carouselTotal = document.querySelectorAll('.carousel')
+for (let i = 0; i < carouselTotal.length; i++) {
+
+const track = carouselTotal[i].querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = carouselTotal[i].querySelector('.carousel__button--right');
+const prevButton = carouselTotal[i].querySelector('.carousel__button--left');
+const dotsNav = carouselTotal[i].querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// arrange the slides next to one another
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 20 + 'px';
+};
+slides.forEach(setSlidePosition);
+
+// function that moves the slide from current to target slide when called
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left; + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+// function that updates the bottom indicator location when slides are moved
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+// function that hides or shows the arrows when you are at the first or last slide
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length -1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
+// when i click right, move slides to the right
+
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+})
+
+// when i click left, move slides to the left
+
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+    
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+})
+
+// when i click the nav indicatos, move to that slide
+
+dotsNav.addEventListener('click', e => {
+    //what indicator was clicked on
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+
+})}
---
+++ assets/js/main.js
@@ -0,0 +1,142 @@
+/*
+	Miniport by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_typewriter
+// i'm sorry in advance for this typewriter code im too lazy the figure how to make one typewriter code that actually continues going
+// i promise to fix it at some point
+
+var i1 = 0; var i2 = 0; var i3 = 0; var i4 = 0; var i5 = 0; var i6 = 0; var i7 = 0; var i8 = 0;
+var i2d = 0; var i5d = 0; var i6d = 0; var i7d = 0; var i8d = 0;
+var msg1 = "mostly paintings, but also other traditional art mediums";
+var msg2 = "every day, for 100 days, i made a zine";
+var msg3 = "inspired by my filipino american heritage";
+var msg4 = "digital labels for Three Streams Brewery";
+var msg5 = "AR app based around different tree frogs";
+var msg6 = "romantic quote resource";
+var msg7 = "an interactive music-making experience";
+var msg8 = "AR project about sharing information on the internet";
+
+var a1 = 0; var a2 = 0;
+var msga1 = "hello! i'm bailey";
+var msga2 = "contact me:";
+
+var date1 = "[ 2019 ]";
+var date2 = "[ 2020 ]";
+var date3 = "[ 2021 ]";
+var date4 = "[ 2022 ]";
+
+
+function typeWritera() {
+	if (a1 < msga1.length) {
+	  document.getElementById("demoa1").innerHTML += msga1.charAt(a1);
+	  a1++;
+	  setTimeout(typeWritera, 100);
+	}}
+
+function typeWriter1() {
+  if (i1 < msg1.length) {
+    document.getElementById("demo1").innerHTML += msg1.charAt(i1);
+    i1++;
+    setTimeout(typeWriter1, 50);
+  }}
+
+function typeWriter2() {
+if (i2 < msg2.length) {
+	document.getElementById("demo2").innerHTML += msg2.charAt(i2);
+	i2++;
+	setTimeout(typeWriter2, 50);
+} else if (i2d < date3.length) {
+	document.getElementById("demo2d").innerHTML += date3.charAt(i2d);
+	i2d++;
+	setTimeout(typeWriter2, 50);
+}}
+
+function typeWriter3() {
+if (i3 < msg3.length) {
+	document.getElementById("demo3").innerHTML += msg3.charAt(i3);
+	i3++;
+	setTimeout(typeWriter3, 50);
+}}
+
+function typeWriter4() {
+if (i4 < msg4.length) {
+	document.getElementById("demo4").innerHTML += msg4.charAt(i4);
+	i4++;
+	setTimeout(typeWriter4, 50);
+}}
+
+function typeWriter5() {
+	if (i5 < msg5.length) {
+		document.getElementById("demo5").innerHTML += msg5.charAt(i5);
+		i5++;
+		setTimeout(typeWriter5, 50);
+	} else if (i5d < date4.length) {
+		document.getElementById("demo5d").innerHTML += date4.charAt(i5d);
+		i5d++;
+		setTimeout(typeWriter5, 50);
+	}}
+
+function typeWriter6() {
+	if (i6 < msg6.length) {
+		document.getElementById("demo6").innerHTML += msg6.charAt(i6);
+		i6++;
+		setTimeout(typeWriter6, 50);
+	} else if (i6d < date3.length) {
+		document.getElementById("demo6d").innerHTML += date3.charAt(i6d);
+		i6d++;
+		setTimeout(typeWriter6, 50);
+	}}
+
+function typeWriter7() {
+	if (i7 < msg7.length) {
+		document.getElementById("demo7").innerHTML += msg7.charAt(i7);
+		i7++;
+		setTimeout(typeWriter7, 50);
+	} else if (i7d < date2.length) {
+		document.getElementById("demo7d").innerHTML += date2.charAt(i7d);
+		i7d++;
+		setTimeout(typeWriter7, 50);
+	}}
+
+function typeWriter8() {
+	if (i8 < msg8.length) {
+		document.getElementById("demo8").innerHTML += msg8.charAt(i8);
+		i8++;
+		setTimeout(typeWriter8, 50);
+	} else if (i8d < date1.length) {
+		document.getElementById("demo8d").innerHTML += date1.charAt(i8d);
+		i8d++;
+		setTimeout(typeWriter8, 50);
+	}}
+
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$nav = $('#nav');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('#nav a, .scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return $nav.height(); }
+		});
+
+})(jQuery);
