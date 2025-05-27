eed9f49ce22a4c090e84ebefaaf0d51c7f4e2012
baileycfoltz <bcf287@nyu.edu>
2025-05-27 17:09:53 -0400

    see if this helps instagram autoplay issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,16 +138,15 @@ function typeWriter10() {
 	}}
 
 
-	function isInstagram() {
-		return navigator.userAgent.includes("Instagram");
-	  }
+	// function isInstagram() {
+	// 	return navigator.userAgent.includes("Instagram");
+	//   }
 	  
-	  if (isInstagram()) {
-		const video = document.querySelector('video');
-		video.removeAttribute('autoplay');
-		video.setAttribute('controls','');
-		// video.removeAttribute('muted'); // Optional: Let users control sound
-	  }
+	//   if (isInstagram()) {
+	// 	const video = document.querySelector('video');
+	// 	video.removeAttribute('autoplay');
+	// 	video.setAttribute('controls','');
+	//   }
 
 
 (function($) {
